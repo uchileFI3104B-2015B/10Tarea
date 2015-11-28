74c36e0a46d07da97e359c39b4062a134062e5f0
Jose Araya <joseguillermo.araya@gmail.com>
2015-11-28 20:47:51 -0300

    primera funcion para modelar

--- forma_gauss.py
+++ forma_gauss.py
@@ -25,9 +25,25 @@ def mostrar_datos(data, xlabel, ylabel, title, ylim):
     plt.savefig("{}.jpg".format(title))
 
 
+def linea(x, m, n):
+    ''' linea recta para aproximación'''
+    return x * m + n
+
+
+def gauss(x, A, mu, sigma):
+    '''función gaussiana para aproximación'''
+    return  A * scipy.stats.norm(loc=mu, scale=sigma).pdf(x)
+
+
+def funcion_1(x, A, mu, sigma, m, n):
+    ''' primera funcion para aproximar'''
+    return linea(x, m, n) + gauss(x, A, mu, sigma)
+
+    
 # main
 wlength, fnu = importar_datos("espectro.dat")
 mostrar_datos([wlength, fnu], "Wavelength",
               "$F_\\nu [erg s^{-1} Hz^{-1} cm^{-2}]$",
               "espectro", [1.28e-16, 1.42e-16])
+
 plt.show()
