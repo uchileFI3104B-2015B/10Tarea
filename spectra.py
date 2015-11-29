6c21638a95a7f387aa0ab38207a578b92efd2177
BScheihing <bruno.scheihing@ing.uchile.cl>
2015-11-28 21:40:37 -0300

    Resuelve fit con Levenberg-Marquardt y plotea resultados

--- spectra.py
+++ spectra.py
@@ -24,11 +24,11 @@ def chi2(data, modelo, params):
     return np.sum(np.power((data[1] - modelo(data[0], params)), 2))
 
 def beta(data, modelo, params):
-    betak = np.zeros((5, len(data[0])))
+    betak = np.zeros(5)
     f1 = data[1] - modelo(data[0], params)
     for k in range(5):
         f2 = deriv_modelo(data[0], params, k, modelo)
-        betak[i] = np.sum(f1*f2)
+        betak[k] = np.sum(f1*f2)
     return betak
 
 def alpha(data, modelo, params, lamb):
@@ -48,3 +48,45 @@ frec = np.loadtxt("espectro.dat", usecols=(1,))
 data = np.zeros((2, len(wavelen)))
 data[0] = wavelen
 data[1] = (10**16)*frec
+coefin = np.polyfit(data[0], data[1], 1)
+act_params1 = (-1, 6565, 5, coefin[0], coefin[1])
+act_params2 = (-1, 6565, 5, coefin[0], coefin[1])
+lamb1 = 0.001
+lamb2 = 0.001
+chi21 = chi2(data, modelo1, act_params1)
+chi22 = chi2(data, modelo2, act_params2)
+deltachi1 = 10
+deltachi2 = 10
+while deltachi1 > 0.00001:
+    alp = alpha(data, modelo1, act_params1, lamb1)
+    bet = beta(data, modelo1, act_params1)
+    delparams = np.dot(np.linalg.inv(alp), bet)
+    chi2t = chi2(data, modelo1, act_params1 + delparams)
+    if chi2t >= chi21:
+        lamb1 = lamb1 * 10
+    else:
+        lamb1 = lamb1 * 0.1
+        act_params1 = act_params1 + delparams
+        deltachi1 = chi21 - chi2t
+        chi21 = chi2t
+
+while deltachi2 > 0.00001:
+    alp = alpha(data, modelo2, act_params2, lamb2)
+    bet = beta(data, modelo2, act_params2)
+    delparams = np.dot(np.linalg.inv(alp), bet)
+    chi2t = chi2(data, modelo2, act_params2 + delparams)
+    if chi2t >= chi22:
+        lamb2 = lamb2 * 10
+    else:
+        lamb2 = lamb2 * 0.1
+        act_params2 = act_params2 + delparams
+        deltachi2 = chi22 - chi2t
+        chi22 = chi2t
+
+plt.figure(1)
+plt.plot(data[0], data[1], 'k')
+plt.plot(data[0], modelo1(data[0], act_params1), 'r')
+plt.figure(2)
+plt.plot(data[0], data[1], 'k')
+plt.plot(data[0], modelo2(data[0], act_params2), 'b')
+plt.show()
