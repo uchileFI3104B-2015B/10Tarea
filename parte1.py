c508483e5f8dbcc7548883b906ccd5d623742a63
LeonardoLeiva <leonardo_conan@hotmail.com>
2015-11-30 19:34:45 -0400

    ajuste gaussiano
    listo el ajuste gaussiano con su grafico

--- parte1.py
+++ parte1.py
@@ -24,24 +24,23 @@ def leer_archivo(nombre):
     return datos
 
 
-def recta(x, p):
+def recta(p, x):
     a, b = p
     return a * x + b
 
 
-def gaussiana(x, p):
+def gaussiana(p, x):
     A, mu, sigma = p
     y = A * scipy.stats.norm(loc=mu, scale=sigma).pdf(x)
     return y
 
 
-def plot(x, y):
+def plot(x, y, y1):
     fig = plt.figure()
     fig.clf()
     ax1 = fig.add_subplot(111)
-    ax1.plot(x, y, 'o')
-    #ax1.plot(x, y1, label="")
-    #ax1.plot(x, y2, label="")
+    ax1.plot(x, y, '+', label="Datos Experimentales")
+    ax1.plot(x, y1, '-', label="Ajuste Gaussiano")
     ax1.plot()
     ax1.set_xlabel("")
     ax1.set_ylabel("")
@@ -51,14 +50,34 @@ def plot(x, y):
     plt.show()
 
 
+def modelo1(p, x):
+    a, b, A, mu, sigma = p
+    p1 = a, b
+    p2 = A, mu, sigma
+    y1 = recta(p1, x)
+    y2 = gaussiana(p2, x)
+    return y1 - y2
+
+
+def experimental():
+    ex = leer_archivo("espectro.dat")
+    x_ex = ex[:, 0]
+    y_ex = ex[:, 1]
+    return x_ex, y_ex
+
+
+def residuo(p, x_exp, y_exp):
+    err = y_exp - modelo1(p, x_exp)
+    return err
+
+
 # inicializacion
-x = np.linspace(-5, 5, 100)
-p1 = 1, 1
-p2 = 1, 0, 1
-y1 = recta(x, p1)
-y2 = gaussiana(x, p2)
-exp = leer_archivo("espectro.dat")
-x_ex = exp[:, 0]
-y_ex = exp[:, 1]
+p1 = 1e-16 , 0
+p2 = 1e-16, 6550, 1
+p0 = p1[0], p1[1], p2[0], p2[1], p2[2]
+x_exp, y_exp = experimental()
+pf = leastsq(residuo, p0, args=(x_exp, y_exp))
 # plot
-plot(x_ex, y_ex)
+p_f = pf[0]
+y1 = modelo1(p_f, x_exp)
+plot(x_exp, y_exp, y1)
