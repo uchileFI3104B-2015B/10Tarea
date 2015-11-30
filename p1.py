dc6b288eb53f71c0445ae4110126201ae7f79756
FernandaPerezV <fer20mc@gmail.com>
2015-11-30 16:43:02 -0300

    se agrega funcion recta_lorentz y se realiza procedimiento analogo. Se agregan las dos figuras de la p1

--- p1.py
+++ p1.py
@@ -1,12 +1,17 @@
 from __future__ import division
 import numpy as np
-from scipy.stats import norm
+import scipy.stats
+from scipy.stats import cauchy
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 
 
-def recta_gaussian(x, a, b, A, mu, sigma):
-    return a * x +b - A * norm(loc=mu, scale=sigma).pdf(x)
+def recta_gaussiana(x, a, b, A, mu, sigma):
+    return a * x + b - A * scipy.stats.norm(loc=mu, scale=sigma).pdf(x)
+
+
+def recta_lorentz(x, a, b, A, mu, sigma):
+    return a * x + b - A * scipy.stats.cauchy(loc=mu, scale=sigma).pdf(x)
 
 
 wavelength = np.loadtxt('espectro.dat', usecols=(0,))
@@ -16,13 +21,34 @@ n = len(wavelength)
 mean = sum(wavelength)/n
 sigma = np.sqrt(sum((wavelength-mean)**2)/n)
 
-popt, pcov = curve_fit(recta_gaussian, wavelength, fnu,
+# primer caso
+popt1, pcov1 = curve_fit(recta_gaussiana, wavelength, fnu,
                        [1e-16, 1e-20, 1e-17, mean, sigma])
 fig = plt.figure(1)
 plt.clf()
 plt.axvline(mean, color='g')
 plt.plot(wavelength, fnu, 'o', label='Datos')
-plt.plot(wavelength, recta_gaussian(wavelength,*popt),'ro:',label='fit')
-plt.legend()
+plt.plot(wavelength, recta_gaussiana(wavelength,*popt1),'ro:',
+         label='Ajuste recta-gaussiana')
+plt.xlabel('Longitud de onda [$\AA$]')
+plt.ylabel('Frecuencia [$erg$ $s^{-1} Hz^{-1} cm^{-2}$]')
+plt.legend(loc=4)
 plt.draw()
 plt.show()
+plt.savefig('figura1.png')
+
+#segundo caso
+popt2, pcov2 = curve_fit(recta_lorentz, wavelength, fnu,
+                       [1e-16, 1e-20, 1e-17, mean, sigma])
+fig = plt.figure(2)
+plt.clf()
+plt.axvline(mean, color='g')
+plt.plot(wavelength, fnu, 'o', label='Datos')
+plt.plot(wavelength, recta_lorentz(wavelength,*popt2),'ro:',
+         label='Ajuste recta-lorentz')
+plt.xlabel('Longitud de onda [$\AA$]')
+plt.ylabel('Frecuencia [$erg$ $s^{-1} Hz^{-1} cm^{-2}$]')
+plt.legend(loc=4)
+plt.draw()
+plt.show()
+plt.savefig('figura2.png')
