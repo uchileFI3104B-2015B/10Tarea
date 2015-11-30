3b04a5eb6ef5be5549e784f7443f586d71a8aa7d
Jose Araya <joseguillermo.araya@gmail.com>
2015-11-30 01:49:41 -0300

    ks test

--- forma_gauss.py
+++ forma_gauss.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
 from scipy.stats import norm
+from scipy.stats import kstest
 from scipy.optimize import (leastsq, curve_fit)
 import pdb
 ''' Script que aproxima el espectro por la suma de una recta y una funcion
@@ -51,6 +52,30 @@ def crear_datos(param_opt):
     return x, y
 
 
+def cdf(data, model):
+    return np.array([np.sum(model <= yy) for yy in data]) / len(model)
+
+
+def generar_prob_acumulada(x_data, y_data, a):
+    '''funcion que recibe los datos y el parametro optimo y genera la
+    distribucion acumulada del modelo'''
+    xmin = np.min(x_data)
+    xmax = np.max(x_data)
+    y_model_sorted = np.sort(funcion_1(np.linspace(xmin, xmax, 1000), *a))
+    y_data_sorted = np.sort(y_data)
+    return y_model_sorted, y_data_sorted
+
+
+def mostrar_prob_acum(f_d, f_m):
+    '''grafica las funciones probabilidad acumulada'''
+    ax, fig = plt.subplots()
+    CDF_model = np.array([np.sum(f_m <= yy) for yy in f_d]) / len(f_m)
+    N = len(f_d)
+    plt.plot(f_d, np.arange(N) / N, '-^', drawstyle='steps-post')
+    plt.plot(f_d, np.arange(1, N+1) / N, '-.', drawstyle='steps-post')
+    plt.plot(f_d, CDF_model, '-x', drawstyle='steps-post')
+
+    
 # main
 wlength, fnu = importar_datos("espectro.dat")
 param_opt, pcov = curve_fit(funcion_1, wlength, fnu,
@@ -61,4 +86,9 @@ print("Parametros optimos recta: Pendiente={}, coef de posicion={}".format(param
 mostrar_datos(wlength, fnu, [x_modelo, y_modelo],  "Wavelength",
               "$F_\\nu [erg s^{-1} Hz^{-1} cm^{-2}]$",
               "Modelo recta + Gaussiana", [1.28e-16, 1.42e-16])
+f_modelo_sorted, f_data_sorted = generar_prob_acumulada(wlength, fnu, param_opt)
+mostrar_prob_acum(f_modelo_sorted, f_data_sorted)
+Dn_scipy, conf = kstest(f_data_sorted, cdf, args=(f_modelo_sorted,))
+print "Dn_scipy   : ", Dn_scipy
+print "Nivel de confianza : ", conf
 plt.show()
