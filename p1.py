5da322d75d35c030d2c92c433717ea9a1c0c2a9f
FernandaPerezV <fer20mc@gmail.com>
2015-11-30 16:47:44 -0300

    se juntan ambas figuras en una sola

--- p1.py
+++ p1.py
@@ -24,31 +24,18 @@ sigma = np.sqrt(sum((wavelength-mean)**2)/n)
 # primer caso
 popt1, pcov1 = curve_fit(recta_gaussiana, wavelength, fnu,
                        [1e-16, 1e-20, 1e-17, mean, sigma])
+popt2, pcov2 = curve_fit(recta_lorentz, wavelength, fnu,
+                       [1e-16, 1e-20, 1e-17, mean, sigma])
 fig = plt.figure(1)
 plt.clf()
-plt.axvline(mean, color='g')
 plt.plot(wavelength, fnu, 'o', label='Datos')
 plt.plot(wavelength, recta_gaussiana(wavelength,*popt1),'ro:',
-         label='Ajuste recta-gaussiana')
+         label='Ajuste recta-gaussiana', alpha=0.4)
+plt.plot(wavelength, recta_lorentz(wavelength,*popt2),'go:',
+         label='Ajuste recta-lorentz', alpha=0.4)
 plt.xlabel('Longitud de onda [$\AA$]')
 plt.ylabel('Frecuencia [$erg$ $s^{-1} Hz^{-1} cm^{-2}$]')
 plt.legend(loc=4)
 plt.draw()
 plt.show()
 plt.savefig('figura1.png')
-
-#segundo caso
-popt2, pcov2 = curve_fit(recta_lorentz, wavelength, fnu,
-                       [1e-16, 1e-20, 1e-17, mean, sigma])
-fig = plt.figure(2)
-plt.clf()
-plt.axvline(mean, color='g')
-plt.plot(wavelength, fnu, 'o', label='Datos')
-plt.plot(wavelength, recta_lorentz(wavelength,*popt2),'ro:',
-         label='Ajuste recta-lorentz')
-plt.xlabel('Longitud de onda [$\AA$]')
-plt.ylabel('Frecuencia [$erg$ $s^{-1} Hz^{-1} cm^{-2}$]')
-plt.legend(loc=4)
-plt.draw()
-plt.show()
-plt.savefig('figura2.png')
