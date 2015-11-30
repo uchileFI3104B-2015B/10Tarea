5792f96f8901b11615a297a5f0c6bcd5ea201716
LeonardoLeiva <leonardo_conan@hotmail.com>
2015-11-30 18:22:25 -0400

    funciones varias
    se hacen algunos test para ver si la generacion de la recta y la
    gaussiana funcionan y si los datos se ven como deberian. se refactoriza
    el proceso de los test en funciones nuevas

--- parte1.py
+++ parte1.py
@@ -8,6 +8,7 @@ este codigo busca modelar un espectro experimental
 from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.stats
 from scipy.optimize import leastsq
 from scipy import optimize as opt
 
@@ -23,5 +24,41 @@ def leer_archivo(nombre):
     return datos
 
 
+def recta(x, p):
+    a, b = p
+    return a * x + b
+
+
+def gaussiana(x, p):
+    A, mu, sigma = p
+    y = A * scipy.stats.norm(loc=mu, scale=sigma).pdf(x)
+    return y
+
+
+def plot(x, y):
+    fig = plt.figure()
+    fig.clf()
+    ax1 = fig.add_subplot(111)
+    ax1.plot(x, y, 'o')
+    #ax1.plot(x, y1, label="")
+    #ax1.plot(x, y2, label="")
+    ax1.plot()
+    ax1.set_xlabel("")
+    ax1.set_ylabel("")
+    plt.legend(loc=4)
+    plt.savefig("a.png")
+    plt.draw()
+    plt.show()
+
+
 # inicializacion
-print leer_archivo("espectro.dat")
+x = np.linspace(-5, 5, 100)
+p1 = 1, 1
+p2 = 1, 0, 1
+y1 = recta(x, p1)
+y2 = gaussiana(x, p2)
+exp = leer_archivo("espectro.dat")
+x_ex = exp[:, 0]
+y_ex = exp[:, 1]
+# plot
+plot(x_ex, y_ex)
