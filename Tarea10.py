f2969bd9e04faabddd6af2a47520ac2694dd2a9b
maxidirk <maxidirk@yahoo.es>
2015-12-01 02:13:10 -0300

    se finaliza p1 y p2 y programa pasa pep8, falta hacer graficos y revision general

--- Tarea10.py
+++ Tarea10.py
@@ -8,10 +8,12 @@ Maximiliano Dirk Vega Aguilera
 '''
 ##############################################################################
 
+from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as scist
 from scipy.optimize import curve_fit
+from scipy.stats import kstwobign
 from scipy.stats import kstest
 
 ##############################################################################
@@ -20,27 +22,27 @@ from scipy.stats import kstest
 
 
 def ajuste_gauss(x, a, b, A, mu, sigma):
-	'''
-	ajuste de una recta menos una gaussiana, los parametros (a,b) pertenecen
-	a la recta y los paramatros (A, mu, sigma) a la gaussiana
-	'''
-	y = (a + b * x) - A * scist.norm(loc=mu, scale=sigma).pdf(x)
-	return y
+    '''
+    ajuste de una recta menos una gaussiana, los parametros (a,b) pertenecen
+    a la recta y los paramatros (A, mu, sigma) a la gaussiana
+    '''
+    y = (a + b * x) - A * scist.norm(loc=mu, scale=sigma).pdf(x)
+    return y
 
 
 def ajuste_lorentz(x, a, b, A, mu, sigma):
-	'''
-	ajuste de una recta menos una perfil de lorentz, los parametros (a,b)
-	pertenecen a la recta y los paramatros (A, mu, sigma) al perfil de lorentz
-	'''
-	y = (a + b * x) - A * scist.cauchy(loc=mu, scale=sigma).pdf(x)
-	return y
+    '''
+    ajuste de una recta menos una perfil de lorentz, los parametros (a,b)
+    pertenecen a la recta y los paramatros (A, mu, sigma) al perfil de lorentz
+    '''
+    y = (a + b * x) - A * scist.cauchy(loc=mu, scale=sigma).pdf(x)
+    return y
 
 
 def obtener_parametros(f, x, y, parametros=False):
     '''
-	entrega los parametros para el ajuste de las funciones anteriores a partir
-	de los datos. Si se le entrega 'parametros' los usa como punto de partida
+    entrega los parametros para el ajuste de las funciones anteriores a partir
+    de los datos. Si se le entrega 'parametros' los usa como punto de partida
     '''
     if parametros is False:
         popt, pcov = curve_fit(f, x, y)
@@ -50,28 +52,35 @@ def obtener_parametros(f, x, y, parametros=False):
         return popt
 
 
+def chi(f, x, y, parametros):
+    chi2 = 0
+    for i in range(len(x)):
+        chi2 += (y[i] - f(x[i], *parametros))**2
+    return chi2
+
+
 def cdf(data, model):
-	'''
-	funcion de distribucion acumulada para los datos segun un modelo.
-	'''
-	return np.array([np.sum(model <= yy) for yy in data]) / len(model)
+    '''
+    funcion de distribucion acumulada para los datos segun un modelo.
+    '''
+    return np.array([np.sum(model <= yy) for yy in data]) / len(model)
 
 
 ##############################################################################
 ##############################################################################
 # P1
 File_espectro = np.loadtxt('espectro.dat')  # se lee archivo
-wavelength = File_espectro[:,0]  # se asigna variable lambda
-fnu = File_espectro[:,1]  # se asigna variable flujo por frecuencia
-parametros = [9.1e-17, 7.4e-21, 1e-17, 6563., 7.]  # parametros iniciales para el ajuste
-#plt.plot(wavelength, fnu, '-o')
-#plt.show()
+wavelength = File_espectro[:, 0]  # se asigna variable lambda
+fnu = File_espectro[:, 1]  # se asigna variable flujo por frecuencia
+# parametros iniciales para el ajuste, obtenidos del grafico de datos
+parametros = [9.1e-17, 7.4e-21, 1e-17, 6563., 7.]
 #############
 # gauss
 #############
-# obtienes los parametros para la funcion ajuste_gauss
-poptg = obtener_parametros(ajuste_gauss, wavelength, fnu, parametros=parametros)
-# Escribir resultados
+# obtener los parametros para la funcion ajuste_gauss
+poptg = obtener_parametros(ajuste_gauss, wavelength,
+                           fnu, parametros=parametros)
+# resultados
 print '----------------------------------------------'
 print 'f(x) = (a + b * x) - A * exp((x - mu) / sigma)'
 print 'a = ', poptg[0]
@@ -79,11 +88,13 @@ print 'b = ', poptg[1]
 print 'A = ', poptg[2]
 print 'mu = ', poptg[3]
 print 'sigma = ', poptg[4]
+print 'chi**2 = ', chi(ajuste_gauss, wavelength, fnu, poptg)
 print '----------------------------------------------'
 # Graficar
 plt.plot(wavelength, ajuste_gauss(wavelength, *poptg), 'r--')
 plt.plot(wavelength, fnu, 'g^')
 plt.axis([6450, 6675, 1.28e-16, 1.42e-16])
+plt.title('Perfil Gaussiano')
 plt.xlabel('$\AA$')
 plt.ylabel('$erg\ \  s^{-1}\ Hz^{-1}\ cm^{-2}$')
 plt.show()
@@ -93,9 +104,10 @@ plt.show()
 # lorentz
 ###############
 
-# Ajustar recta-gaussiana
-poptl = obtener_parametros(ajuste_lorentz, wavelength, fnu, parametros=parametros)
-# Escribir resultados
+# obtener los parametros para la funcion ajuste_lorentz
+poptl = obtener_parametros(ajuste_lorentz, wavelength,
+                           fnu, parametros=parametros)
+# resultados
 print '----------------------------------------------'
 print 'f(x) = (a + b * x) - A / (pi * (1 + (x - mu)**2 / sigma**2))'
 print 'a = ', poptl[0]
@@ -103,11 +115,13 @@ print 'b = ', poptl[1]
 print 'A = ', poptl[2]
 print 'mu = ', poptl[3]
 print 'sigma = ', poptl[4]
+print 'chi**2 = ', chi(ajuste_lorentz, wavelength, fnu, poptl)
 print '----------------------------------------------'
 # Graficar
 plt.plot(wavelength, ajuste_lorentz(wavelength, *poptl), 'r--')
 plt.plot(wavelength, fnu, 'g^')
 plt.axis([6450, 6675, 1.28e-16, 1.42e-16])
+plt.title('Perfil Lorentziano')
 plt.xlabel('$\AA$')
 plt.ylabel('$erg\ \  s^{-1}\ Hz^{-1}\ cm^{-2}$')
 plt.show()
@@ -120,6 +134,7 @@ plt.plot(wavelength, ajuste_gauss(wavelength, *poptg), 'r--')
 plt.plot(wavelength, ajuste_lorentz(wavelength, *poptl), 'g--')
 plt.plot(wavelength, fnu, 'y^')
 plt.axis([6450, 6675, 1.28e-16, 1.42e-16])
+plt.title('Perfil Gaussiano vs Perfil Lorentziano')
 plt.xlabel('$\AA$')
 plt.ylabel('$erg\ \  s^{-1}\ Hz^{-1}\ cm^{-2}$')
 plt.show()
@@ -129,8 +144,7 @@ plt.show()
 # P2
 
 N = 1e+5  # numero de datos a modelar
-parametros = [9.1e-17, 7.4e-21, 1e-17, 6563., 7.]  # parametros iniciales para el ajuste
-
+n = len(fnu)  # largo de los datos
 # determina valores extremos de la longitud de onda
 xmin = np.min(wavelength)  # minimo wavelength
 xmax = np.max(wavelength)  # maximo wavelength
@@ -139,30 +153,98 @@ xmax = np.max(wavelength)  # maximo wavelength
 # gauss
 ##########
 
-parametros_gauss = obtener_parametros(ajuste_gauss, wavelength, fnu, parametros=parametros)
+parametros_gauss = obtener_parametros(ajuste_gauss, wavelength, fnu,
+                                      parametros=parametros)
 
 # crea y ordena datos fnu a partir del modelo
-fnu_modelo_gauss = np.sort(ajuste_gauss(np.linspace(xmin, xmax, N), *parametros_gauss))
+fnu_modelo_gauss = np.sort(ajuste_gauss(np.linspace(xmin, xmax, N),
+                           *parametros_gauss))
 fnu_datos_gauss = np.sort(fnu)  # ordena los valores fnu
-cdf_gauss = np.array([np.sum(fnu_modelo_gauss <= yy) \
-					 for yy in fnu_datos_gauss]) / len(fnu_modelo_gauss)
-n = len(fnu)
-plt.plot(fnu_datos_gauss, np.arange(n) / n, '-^', drawstyle = 'steps-post')
-plt.plot(fnu_datos_gauss, np.arange(1, n + 1) / n, '-.', drawstyle = 'steps-post')
-plt.plot(fnu_datos_gauss, cdf_gauss, '-x', drawstyle = 'steps-post')
+cdf_gauss = np.array([np.sum(fnu_modelo_gauss <= yy)
+                     for yy in fnu_datos_gauss]) / len(fnu_modelo_gauss)
+
+plt.plot(fnu_datos_gauss, np.arange(n) / n, '-^', drawstyle='steps-post')
+plt.plot(fnu_datos_gauss, np.arange(1, n + 1) / n, '-.',
+         drawstyle='steps-post')
+plt.plot(fnu_datos_gauss, cdf_gauss, '-x', drawstyle='steps-post')
+plt.title('CDF Gauss')
+plt.xlabel('')
+plt.ylabel('')
 plt.show()
 
+# obtiene valores maximos de la distancia entre cdf de los datos y el modelo
 max1g = np.max(cdf_gauss - np.arange(n) / n)
-max2g = np.max(np.arange(1,n + 1) / n - cdf_gauss)
+max2g = np.max(np.arange(1, n + 1) / n - cdf_gauss)
+# guarda el maximo de los maximos
 Dng = max(max1g, max2g)
-print Dng
+print '----------------------------------------------'
+print 'Gauss'
+print 'Distacia Maxima entre CDF\'s =', Dng
 
+ks_dist_g = kstwobign()  # crea el objeto de Kolmogorov-Smirnov two-sided test
+alpha = 0.05  # tolerancia 5%
+# obtiene la distancia critica como el punto en 95% de la distribucion
+Dng_crit = ks_dist_g.ppf(1-alpha) / np.sqrt(n)
 
-Dng_testg, prob_scipyg = kstest(fnu_datos_gauss, cdf, args=(fnu_modelo_gauss,))
+print 'Distacia critica al 5% =', Dng_crit
 
-print Dng_testg
-print prob_scipyg
+print 'Nivel de confianza =', 1 - ks_dist_g.cdf(Dng*np.sqrt(n))
 
+# test ya implementado
+Dn_testg, prob_scipyg = kstest(fnu_datos_gauss, cdf, args=(fnu_modelo_gauss,))
+
+print 'Distacia Maxima entre CDF\'s implementado =', Dn_testg
+print 'Nivel de confianza implementado =', prob_scipyg
+print '----------------------------------------------'
+
+###############
+# lorentz
+###############
+
+parametros_lorentz = obtener_parametros(ajuste_lorentz, wavelength, fnu,
+                                        parametros=parametros)
+
+# crea y ordena datos fnu a partir del modelo
+fnu_modelo_lorentz = np.sort(ajuste_lorentz(np.linspace(xmin, xmax, N),
+                             *parametros_lorentz))
+fnu_datos_lorentz = np.sort(fnu)  # ordena los valores fnu
+cdf_lorentz = np.array([np.sum(fnu_modelo_lorentz <= yy)
+                       for yy in fnu_datos_lorentz]) / len(fnu_modelo_lorentz)
+
+plt.plot(fnu_datos_lorentz, np.arange(n) / n, '-^', drawstyle='steps-post')
+plt.plot(fnu_datos_lorentz, np.arange(1, n + 1) / n, '-.',
+         drawstyle='steps-post')
+plt.plot(fnu_datos_lorentz, cdf_lorentz, '-x', drawstyle='steps-post')
+plt.title('CDF Lorentz')
+plt.xlabel('')
+plt.ylabel('')
+plt.show()
+
+# obtiene valores maximos de la distancia entre cdf de los datos y el modelo
+max1l = np.max(cdf_lorentz - np.arange(n) / n)
+max2l = np.max(np.arange(1, n + 1) / n - cdf_lorentz)
+# guarda el maximo de los maximos
+Dnl = max(max1l, max2l)
+print '----------------------------------------------'
+print 'Lorentz'
+print 'Distacia Maxima entre CDF\'s =', Dnl
+
+ks_dist_l = kstwobign()  # crea el objeto de Kolmogorov-Smirnov two-sided test
+alpha = 0.05  # tolerancia 5%
+# obtiene la distancia critica como el punto en 95% de la distribucion
+Dnl_crit = ks_dist_l.ppf(1-alpha) / np.sqrt(n)
+
+print 'Distacia critica al 5% =', Dng_crit
+
+print 'Nivel de confianza =', 1 - ks_dist_l.cdf(Dnl*np.sqrt(n))
+
+# test ya implementado
+Dn_testl, prob_scipyl = kstest(fnu_datos_lorentz, cdf,
+                               args=(fnu_modelo_lorentz,))
+
+print 'Distacia Maxima entre CDF\'s implementado =', Dn_testl
+print 'Nivel de confianza implementado =', prob_scipyl
+print '----------------------------------------------'
 
 ##############################################################################
 ##############################################################################
