8f670de4f77b5bf56fa7055b66de8daa8783857e
FernandaPerezV <fer20mc@gmail.com>
2015-12-01 18:04:32 -0300

    se agregan funciones chi_gauss y chi_lorentz y se implementan para que entregen el valor con los mejores parametros enconttrados

--- p1.py
+++ p1.py
@@ -23,18 +23,27 @@ def recta_lorentz(x, a, b, A, mu, sigma):
     return a * x + b - A * scipy.stats.cauchy(loc=mu, scale=sigma).pdf(x)
 
 
+def chi_gauss(x, y, a, b, A, mu, sigma):
+    return sum((y - recta_gaussiana(x, a, b, A, mu, sigma))**2)
+
+
+def chi_lorentz(x, y, a, b, A, mu, sigma):
+    return sum((y - recta_lorentz(x, a, b, A, mu, sigma))**2)
+
+
 wavelength = np.loadtxt('espectro.dat', usecols=(0,))
 fnu = np.loadtxt('espectro.dat', usecols=(1,))
 
 n = len(wavelength)
 mean = sum(wavelength)/n
 sigma = np.sqrt(sum((wavelength-mean)**2)/n)
+m, n = np.polyfit(wavelength, fnu, 1)
 
 
 popt1, pcov1 = curve_fit(recta_gaussiana, wavelength, fnu,
-                         [1e-16, 1e-20, 1e-17, mean, sigma])
+                         [m, n, 1e-17, mean, sigma])
 popt2, pcov2 = curve_fit(recta_lorentz, wavelength, fnu,
-                         [1e-16, 1e-20, 1e-17, mean, sigma])
+                         [m, n, 1e-17, mean, sigma])
 fig = plt.figure(1)
 plt.clf()
 plt.plot(wavelength, fnu, 'o', label='Datos', alpha=0.4, color='g')
@@ -54,3 +63,8 @@ print 'mejores parametros caso1'
 print popt1
 print 'mejores parametros caso 2'
 print popt2
+
+print 'valor chi cuadrado gaussiana:'
+print chi_gauss(wavelength, fnu, *popt1)
+print 'valor chi cuadrado lorentz:'
+print chi_lorentz(wavelength, fnu, *popt2)
