20e08aac4370ebb8b2f6262d89e6a1bdf07c87b0
LeonardoLeiva <leonardo_conan@hotmail.com>
2015-11-30 21:38:40 -0400

    parte 2
    se realiza el test de K-S. las funciones son muy similares a la parte 1
    y las que no se obtienen del demo. solo se cambian algunos parametros.
    por eso no hay commits parte por parte, salio bastante rapido el
    resultado

---
+++ parte2.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+este codigo busca verificar si los modelos de gauss y/o lorentz son adecuados
+para los datos experimentales.
+'''
+
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats
+from scipy.optimize import leastsq
+from scipy import optimize as opt
+from scipy.stats import kstest
+
+# funciones estructurales
+
+
+def leer_archivo(nombre):
+    '''
+    lee el archivo
+    nombre debe ser un str
+    '''
+    datos = np.loadtxt(nombre)
+    return datos
+
+
+def modelo_gauss(p, x):
+    a, b, A, mu, sigma = p
+    y1 = a * x + b
+    y2 = y = A * scipy.stats.norm(loc=mu, scale=sigma).pdf(x)
+    return y1 - y2
+
+
+def modelo_lorentz(p, x):
+    a, b, A, mu, sigma = p
+    y1 = a * x + b
+    y2 = A * scipy.stats.cauchy(loc=mu, scale=sigma).pdf(x)
+    return y1 - y2
+
+
+def experimental():
+    ex = leer_archivo("espectro.dat")
+    x_ex = ex[:, 0]
+    y_ex = ex[:, 1]
+    return x_ex, y_ex
+
+
+def residuo_gauss(p, x_exp, y_exp):
+    err = y_exp - modelo_gauss(p, x_exp)
+    return err
+
+
+def residuo_lorentz(p, x_exp, y_exp):
+    err = y_exp - modelo_lorentz(p, x_exp)
+    return err
+
+
+def cdf(data, modelo):
+    return np.array([np.sum(modelo <= yy) for yy in data]) / len(modelo)
+
+
+def prueba_ks(x_exp, y_exp, modelo, p_opt):
+    xmin = np.min(x_exp)
+    xmax = np.max(x_exp)
+    y_modelo_ord = np.sort(modelo(p_opt, np.linspace(xmin, xmax, 1000)))
+    y_exp_ord = np.sort(y_exp)
+    Dn, prob = kstest(y_exp_ord, cdf, args=(y_modelo_ord,))
+    return Dn, prob
+
+
+# main
+p0 = 1e-16 , 0, 1e-16, 6550, 1
+x_exp, y_exp = experimental()
+aprox_1 = leastsq(residuo_gauss, p0, args=(x_exp, y_exp))
+aprox_2 = leastsq(residuo_lorentz, p0, args=(x_exp, y_exp))
+opt1 = aprox_1[0]
+opt2 = aprox_2[0]
+print prueba_ks(x_exp, y_exp, modelo_gauss, opt1)
+print prueba_ks(x_exp, y_exp, modelo_lorentz, opt2)
