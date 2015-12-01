5e29bb3d83f56cd8f7c68e4085202183141ddebc
Benguerrero1 <benjaminguerrero1@gmail.com>
2015-12-01 11:50:47 -0300

    Se limpian las funciones definidas, se importa la funcion cdf del demo, y se

--- P1.py
+++ P1.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 from scipy import stats
 import numpy as np
+from scipy.optimize import (leastsq, curve_fit)
 
 """
-Created on Tue Dec 01 00:14:40 2015
-
-@author: Administrador
+Este programa extrae los datos de espectro.dat, modela el espectro
+segun una perfil de Lorentz y Gauss, y ve cual es el mejor modelo
+usando K-S.
 """
 
-def gauss(A, mu, sigma ,x):
+def gauss(A, mu, sigma, x):
     g = A * stats.norm(loc=mu, scale=sigma).pdf(x)
     return g
     
@@ -19,10 +20,12 @@ def lorentz(A, mu, sigma, x):
 def linea(x, m, n):
     return x * m + n
         
-def modelo_gauss(x, A, mu, sigma, m, n):
+def modelo_g(x, param):
+    A, mu, sigma, m, n = param
     return linea(x, m, n) - gauss(A, mu, sigma, x)
     
-def modelo_lorentz(x, A, mu, sigma, m, n):
+def modelo_l(x, param):
+    A, mu, sigma, m, n = param
     return linea(x, m, n) - lorentz(A, mu, sigma, x)
     
 def chi_2(datos, modelo, param):
@@ -32,9 +35,24 @@ def chi_2(datos, modelo, param):
     param es un array que contiene (A, mu, sigma, m y n).
     '''
     x_m = datos[0]
-    y_m = modelo(x_m, param[0], param[1], param[2], param[3], param[4])
+    y_m = modelo(x_m, param)
     return np.sum((datos[1] - y_m) ** 2)
+    
+def prob_acumulada(datos, modelo):
+    '''
+    Esto es para hacer K-S usando scipy.
+    '''
+    return np.array([np.sum(modelo <= yy) for yy in datos]) / len(modelo)
+    
+
 
 #Main
     
-np.loadtxt('espectro.dat')
+d1 = np.loadtxt('espectro.dat')
+l_onda = d1[:,0]
+f_nu = d1[:,1]
+
+param_opt_l, pcov = curve_fit(modelo_l, l_onda, f_nu,
+                            p0)
+param_opt_g, pcov = curve_fit(modelo_g, l_onda, f_nu,
+                            p0)
