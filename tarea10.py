6b77a6b407c8e02f7a812d1092d1a806f5e117df
TatiFlores <tatiflores.4@gmail.com>
2015-12-01 17:13:44 -0300

    Calcula chi cuadrado

--- tarea10.py
+++ tarea10.py
@@ -44,6 +44,16 @@ def modelo_2(x, m, n, A, mu, sigma):
     parametros_lorentz = [A, mu, sigma]
     return recta(parametros_recta, x) - Lorentz(parametros_lorentz, x)
 
+def chi2(data, parametros_modelo, funcion_modelo):
+    x_datos = data[0]
+    y_datos = data[1]
+    m, n, A, mu, sigma = parametros_modelo
+    y_modelo = funcion_modelo(x_datos, m, n, A, mu, sigma)
+    chi2 = (y_datos - y_modelo)**2
+    return np.sum(chi2)
+
+
+# Main
 # Datos
 wavelength = np.loadtxt("espectro.dat", usecols=[0])
 Fnu = np.loadtxt("espectro.dat", usecols=[1])
@@ -53,13 +63,20 @@ adivinanza = [1e-20, 1.39*(1e-16), 0.1*(1e-16), 6570, 1]  # m, n, A, mu, sigma
 # Modelo 1
 param_optimo1, param_covar1 = curve_fit(modelo_1, wavelength, Fnu, adivinanza)
 m1, n1, A1, mu1, sigma1 = param_optimo1
+chi2_1 = chi2([wavelength, Fnu], param_optimo1, modelo_1)
 
 # Modelo 2
 param_optimo2, param_covar2 = curve_fit(modelo_2, wavelength, Fnu, adivinanza)
 m2, n2, A2, mu2, sigma2 = param_optimo2
+chi2_2 = chi2([wavelength, Fnu], param_optimo2, modelo_2)
 
-print param_optimo1
-print param_optimo2
+print 'Primer modelo: recta menos gaussiana'
+print '[m, n, A, mu, sigma]:',param_optimo1
+print 'Chi2 :', chi2_1
+print ''
+print 'Segundo modelo: recta menos perfil de Lorentz'
+print '[m, n, A, mu, sigma]:',param_optimo2
+print 'Chi2 :', chi2_2
 
 x = np.linspace(min(wavelength), max(wavelength), 100)
 
