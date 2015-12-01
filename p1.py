ed032a858c535ede76c702ece4d7d439f60359a8
constanzaurzua <urzuacisterna@gmail.com>
2015-12-01 19:25:57 -0300

    termino de codigo

--- p1.py
+++ p1.py
@@ -50,21 +50,56 @@ def chi_cuadrado(x_data,y_data, modelo_1, a0):
     chi_2 = (y_data - y_modelo) ** 2
     return np.sum(chi_2)
 
+
+def cdf(data, model):
+    return np.array([np.sum(model <= yy) for yy in data]) / len(model)
+
+def dn(x_data,y_data,a_opt,a_opt1):
+    xmin = np.min(x_data)
+    xmax = np.max(x_data)
+    y_model_sorted = np.sort(modelo_1(np.linspace(xmin, xmax, 1000), *a_opt))
+    y_model_sorted1 = np.sort(modelo_2(np.linspace(xmin, xmax, 1000), *a_opt1))
+    y_data_sorted = np.sort(y_data)
+    N = len(y_data_sorted)
+    Dn_scipy, prob_scipy = kstest(y_data_sorted, cdf, args=(y_model_sorted,))
+    Dn_scipy2, prob_scipy2 = kstest(y_data_sorted, cdf, args=(y_model_sorted1,))
+
+    ks_dist = kstwobign() # two-sided, aproximacion para big N
+    alpha = 0.05
+    Dn_critico = ks_dist.ppf(1 - alpha) / np.sqrt(N)
+    print "Dn_critico = ", Dn_critico
+    print "Dn para nuestro fit = ", Dn_scipy
+    print "nivel de confianza",prob_scipy
+    print "Dn para nuestro fit = ", Dn_scipy2
+    print "nivel de confianza =",prob_scipy2
+
+
+
+
+
+
+#calculo de dn
+N1 = 10000
+x = np.linspace(6400,6700,N1)
 datos = np.loadtxt('espectro.dat')
 x_data = datos[:,0]
 y_data = datos[:,1]
-N1 = 10000
+
 a0 = 1.3e-16, 3.2996632996633799e-20, 0.1e-16, 6560, 1
 a_opt, a_covar = curve_fit(modelo_1, x_data, y_data, a0)
 a_opt1, a_covar1 = curve_fit(modelo_2, x_data, y_data, a0)
-a = chi_cuadrado(x_data,y_data, modelo_1, a0)
-b= chi_cuadrado(x_data,y_data, modelo_2, a0)
-print "chi modelo Gaussiana =",a
-print "Chi modelo Lorenz =",b
+chi1 = chi_cuadrado(x_data,y_data, modelo_1, a0)
+chi2= chi_cuadrado(x_data,y_data, modelo_2, a0)
+
+
+#resultados
+print "chi modelo Gaussiana =",chi1
+print "Chi modelo Lorenz =",chi2
+dn(x_data,y_data,a_opt,a_opt1)
 
 
 #plots
-x = np.linspace(6400,6700,N1)
+
 plt.plot(x_data,y_data,'co-')
 plt.plot(x, modelo_1(x, *a_opt))
 plt.plot(x, modelo_1(x, *a_opt1),color='deeppink')
