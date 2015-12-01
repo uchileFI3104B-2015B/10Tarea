b465fe75824517727abf18949cc2a5ed19bd50ad
benjammin94 <benja_hunter_x@hotmail.com>
2015-12-01 00:06:28 -0300

    Update 2
    Implemente p1

--- codigo.py
+++ codigo.py
@@ -22,7 +22,13 @@ def gauss(params, x):
     return A * scipy.stats.norm(loc=mu, scale=sigma).pdf(x)
 
 
-def f_modelo(params, x):
+def lorentz(params, x):
+    A = params[0]
+    mu = params[1]
+    sigma = params[2]
+    return A * scipy.stats.cauchy(loc=mu, scale=sigma).pdf(x)
+
+def f_modelo_g(params, x):
     a, b, A, mu, sigma = params
     params_f = a, b
     params_g = A, mu, sigma
@@ -30,10 +36,25 @@ def f_modelo(params, x):
     f_gauss = gauss(params_g, x)
     return f_recta -  f_gauss
 
+def f_modelo_l(params, x):
+    a, b, A, mu, sigma = params
+    params_f = a, b
+    params_l = A, mu, sigma
+    f_recta = recta(params_f, x)
+    f_lorentz = lorentz(params_l, x)
+    return f_recta -  f_lorentz
 
-def f_minimizar(a, b, A, mu, sigma, x):
+
+
+
+def f_minimizar_g(x, a, b, A, mu, sigma):
     params = a, b, A, mu, sigma
-    return f_modelo(params, x)
+    return f_modelo_g(params, x)
+
+
+def f_minimizar_l(x, a, b, A, mu, sigma):
+    params = a, b, A, mu, sigma
+    return f_modelo_l(params, x)
 
 
 #Main
@@ -42,33 +63,48 @@ wave_length = datos[:,0]
 f_nu = datos[:,1]
 
 ad_a = 0
-ad_b = 8.87e-17
-ad_A = 8.22e-17
-ad_mu = 6563
-ad_sigma = 3
+ad_b = 1.4e-16
+ad_A = 0.1e-16
+ad_mu = 6580
+ad_sigma = 10
 
 adivinanzas = ad_a, ad_b, ad_A, ad_mu, ad_sigma
 
-params, cov = curve_fit(f_minimizar, wave_length, f_nu, adivinanzas)
+params_g, cov_g = curve_fit(f_minimizar_g, wave_length, f_nu, adivinanzas)
+params_l, cov_l = curve_fit(f_minimizar_l, wave_length, f_nu, adivinanzas)
 
-a = params[0]
-b = params[1]
-A = params[2]
-mu = params[3]
-sigma = params[4]
+a_g = params_g[0]
+b_g = params_g[1]
+A_g = params_g[2]
+mu_g = params_g[3]
+sigma_g = params_g[4]
 
-print "Parametros a, b:", a, ", ", b
-print "Parametros A, mu, sigma:", A, ", ", mu, ", ", sigma
+a_l = params_l[0]
+b_l = params_l[1]
+A_l = params_l[2]
+mu_l = params_l[3]
+sigma_l = params_l[4]
 
-y_optimo = f_modelo(params, wave_length)
+print "-------------------------------------------------------"
+print "Gauss:"
+print "Parametros a, b:", a_g, ", ", b_g
+print "Parametros A, mu, sigma:", A_g, ", ", mu_g, ", ", sigma_g
+print "-------------------------------------------------------"
+print "Lorentz"
+print "Parametros a, b:", a_l, ", ", b_l
+print "Parametros A, mu, sigma:", A_l, ", ", mu_l, ", ", sigma_l
+
+y_optimo_g = f_modelo_g(params_g, wave_length)
+y_optimo_l = f_modelo_l(params_l, wave_length)
 
 fig = plt.figure()
 fig.clf()
 ax1 = fig.add_subplot(111)
 ax1.plot(wave_length, f_nu, '*', label="Datos Experimentales")
-ax1.plot(wave_length, y_optimo, label="Ajuste Gaussiano-Recta")
-ax1.set_xlabel("Wave length")
-ax1.set_ylabel("Fnu")
+ax1.plot(wave_length, y_optimo_g, label="Ajuste Gaussiano-Recta")
+ax1.plot(wave_length, y_optimo_l, label="Ajuste Lorentz-Recta")
+ax1.set_xlabel("Longitud de onda $[Angstrom]$")
+ax1.set_ylabel("Flujo por unidad de frecuencia [$erg/s/Hz/cm^2}$]")
 plt.legend(loc=4)
 plt.draw()
 plt.show()
