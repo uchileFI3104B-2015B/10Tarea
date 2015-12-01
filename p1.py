c0cfa31ece783883849557b25b0ce62bf1f726a1
hpozojose <hpozo.jose@gmail.com>
2015-12-01 19:00:11 -0300

    agrega varianza y dn

--- p1.py
+++ p1.py
@@ -9,6 +9,7 @@ from scipy.optimize import (leastsq, curve_fit)
 from scipy import optimize as opt
 from scipy.stats import kstest
 import os
+from scipy.stats import kstwobign
 
 '''
 Este script modela 5 parametros a la vez a partir de los datos de
@@ -92,10 +93,56 @@ def grafico(x_data, y_data, ag_opt, al_opt):
     plt.show()
 
 
+def chi_cuadrado(x_data, y_data, ag, al):
+    '''recibe los datos y el modelo a evaluar,
+    devuleve el valor de chi cuadrado'''
+    y_lorentz = espectroLorentz(x_data, *al)
+    y_gauss = espectroGauss(x_data, *ag)
+    chi_2_g = (y_data - y_gauss) ** 2
+    chi_2_l = (y_data - y_lorentz) ** 2
+    return chi_2_g, chi_2_l
+
+
+def cdf(data, model):
+    return np.array([np.sum(model <= yy) for yy in data]) / len(model)
+
+
+def dn(x_data, y_data, ag, al):
+    xmin = np.min(x_data)
+    xmax = np.max(x_data)
+    yg_model_sorted = np.sort(espectroGauss(np.linspace(xmin, xmax, 1000), *ag))
+    yl_model_sorted = np.sort(espectroLorentz(np.linspace(xmin, xmax, 1000),
+                                              *al))
+    y_data_sorted = np.sort(y_data)
+    N = len(y_data_sorted)
+    # Determina Dn critico
+    ks_dist = kstwobign() # two-sided, aproximacion para big N
+    alpha = 0.05
+    Dn_critico = ks_dist.ppf(1 - alpha) / np.sqrt(N)
+    # Determina Dn de los modelos
+    Dn_g_scipy, prob_g_scipy = kstest(y_data_sorted, cdf,
+                                      args=(yg_model_sorted,))
+    Dn_l_scipy, prob_l_scipy = kstest(y_data_sorted, cdf,
+                                      args=(yl_model_sorted,))
+    print "Dn_critico = ", Dn_critico
+    print "Dn_scipy con Gaussiana  : ", Dn_g_scipy
+    print "Nivel de confianza con Gaussiana : ", prob_g_scipy
+    print "Dn_scipy con Perfil de Lorentz  : ", Dn_l_scipy
+    print "Nivel de confianza con Perfil de Lorentz: ", prob_l_scipy
+
+
 
 # Main
 
+# descarga datos
 longitud, flujo = datos()
+# condiciones iniciales
 A0 = 1e-20, 1 * 1e-16, 1 * 1e-16, 6570, 1
+# constantes optimas
 AG_opt, AL_opt = mejor_fit(longitud, flujo, A0)
+# plots
 grafico(longitud, flujo, AG_opt, AL_opt)
+#calculo chi cuadrado para cada modelo
+chig, chil = chi_cuadrado(longitud, flujo, AG_opt, AL_opt)
+# dn
+dn(longitud, flujo, AG_opt, AL_opt)
