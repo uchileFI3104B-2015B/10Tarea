f00ddfd93ec40e3f034dd8d62bbeb899d0dc043e
luzaguero <luz.francisca.ac@gmail.com>
2015-12-01 20:58:58 -0300

    PEP8

--- Tarea10.py
+++ Tarea10.py
@@ -5,6 +5,7 @@ import scipy.stats
 from scipy.optimize import leastsq, curve_fit
 from scipy.stats import kstest
 
+
 def datos():
     '''
     Lee el archivo y retorna sus columnas como 2 listas
@@ -14,21 +15,24 @@ def datos():
     flux = arch[:, 1]
     return wave, flux
 
+
 def rectagauss(x, a, b, A, mu, sigma):
     '''
     Retorna el ajuste para un modelo gaussiano
     '''
     recta = a + b * x
-    gauss = A * scipy.stats.norm(loc = mu, scale = sigma).pdf(x)
-    return  recta - gauss
+    gauss = A * scipy.stats.norm(loc=mu, scale=sigma).pdf(x)
+    return recta - gauss
+
 
 def rectalorentz(x, a, b, A, mu, sigma):
     '''
     Retorna el ajuste para un modelo lorentziano
     '''
     recta = a + b * x
-    lorentz = A * scipy.stats.cauchy(loc = mu, scale = sigma).pdf(x)
+    lorentz = A * scipy.stats.cauchy(loc=mu, scale=sigma).pdf(x)
     return recta - lorentz
+
 
 def fit(funcion, x, y, seeds):
     '''
@@ -37,6 +41,7 @@ def fit(funcion, x, y, seeds):
     '''
     popt, pcov = curve_fit(funcion, x, y, seeds)
     return popt
+
 
 def chi(funcion, x, y, seeds_op):
     '''
@@ -73,12 +78,12 @@ def test_ks(wave, flux, p_optg, p_optl):
     xmin = np.min(wave)
     xmax = np.max(wave)
     x = np.linspace(xmin, xmax, 1000)
-    #Gauss
+    # Gauss
     y_gauss_ord = np.sort(rectagauss(x, *p_optg))
     y_expg_ord = np.sort(flux)
     dng, probg = kstest(y_expg_ord, cdf, args=(y_gauss_ord,))
 
-    #Lorentz
+    # Lorentz
     y_lorentz_ord = np.sort(rectalorentz(x, *p_optl))
     y_expl_ord = np.sort(flux)
     dnl, probl = kstest(y_expl_ord, cdf, args=(y_lorentz_ord,))
@@ -89,16 +94,20 @@ def test_ks(wave, flux, p_optg, p_optl):
 # Setup
 wave, flux = datos()
 n = len(wave)
-seeds = 9.e-17, 8.e-21, 1.e-17, 6550., 10. # Semillas para el ajuste, sacadas del grafico espectro (a, b, A, mu, sigma)
+
+# Semillas para el ajuste, sacadas del grafico espectro (a, b, A, mu, sigma)
+seeds = 9.e-17, 8.e-21, 1.e-17, 6550., 10.
 
 # Main
 poptg = fit(rectagauss, wave, flux, seeds)  # Gaussiana
 poptl = fit(rectalorentz, wave, flux, seeds)  # Lorentziana
 
 print 'Modelo Gauss:'
-print 'a =', poptg[0], 'b =', poptg[1], 'A =', poptg[2], 'mu =', poptg[3], 'sigma =', poptg[4], 'chi**2 = ', chi(rectagauss, wave, flux, poptg)
+print 'a =', poptg[0], 'b =', poptg[1], 'A =', poptg[2], 'mu =', poptg[3],\
+    'sigma =', poptg[4], 'chi**2 = ', chi(rectagauss, wave, flux, poptg)
 print 'Modelo Lorentz:'
-print 'a =', poptl[0], 'b =', poptl[1], 'A =', poptl[2], 'mu =', poptl[3], 'sigma =', poptl[4], 'chi**2 = ', chi(rectalorentz, wave, flux, poptl)
+print 'a =', poptl[0], 'b =', poptl[1], 'A =', poptl[2], 'mu =', poptl[3],\
+    'sigma =', poptl[4], 'chi**2 = ', chi(rectalorentz, wave, flux, poptl)
 
 optimog = leastsq(errorgauss, seeds, args=(wave, flux))[0]
 optimol = leastsq(errorlorentz, seeds, args=(wave, flux))[0]
@@ -112,8 +121,10 @@ fig = plt.figure()
 ax1 = fig.add_subplot(111)
 
 ax1.plot(wave, flux, 'y*-', label="Datos Experimentales")
-ax1.plot(wave, rectalorentz(wave, *poptl), 'r-', linewidth = '1.5', label="Ajuste Lorentz")
-ax1.plot(wave, rectagauss(wave, *poptg), 'g-', linewidth = '1.5', label="Ajuste Gauss")
+ax1.plot(wave, rectalorentz(wave, *poptl), 'r-', linewidth='1.5',
+         label="Ajuste Lorentz")
+ax1.plot(wave, rectagauss(wave, *poptg), 'g-', linewidth='1.5',
+         label="Ajuste Gauss")
 ax1.set_xlabel("Longitud de onda $[Angstrom]$")
 ax1.set_ylabel("Flujo por unidad de frecuencia $[erg s^{-1} Hz^{-1} cm^{-2}]$")
 ax1.set_title("Espectro y sus ajustes")
