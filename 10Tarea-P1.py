5be9c1fa60034b010f55226c2421042a29437b26
Felipe Castillo <felipe.castillo.t@ing.uchile.cl>
2015-12-02 01:20:03 -0300

    comienzo del script para P1

---
+++ 10Tarea-P1.py
@@ -0,0 +1,12 @@
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Script para encontrar modelar con perfiles de gauss y de lorentz  un segmento
+de un espectro de una fuente que muestra continuo y una linea de absorcion
+'''
+from __future__ import division
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
