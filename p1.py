900bdc9212176a7643ca97f2466b41324d5692e8
sebastiantp <sebastian.toro@ug.uchile.cl>
2015-12-02 18:14:04 -0300

    terminada

---
+++ p1.py
@@ -0,0 +1,7 @@
+from __future__ import division
+import numpy as np
+import numpy.random as npr
+import matplotlib.pyplot as plt
+from scipy.optimize import leastsq
+import scipy.stats
+from scipy.stats import kstest
